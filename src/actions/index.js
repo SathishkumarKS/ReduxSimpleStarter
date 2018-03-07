7be7f9d39f16c6e99c51553ea6932fc6b0d6c429
Sathishkumar K <sathish.hari@gmail.com>
2018-03-07 13:49:47 +0530

    Add PostDetail view and back link to Posts index view

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const BASE_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=Mu6fRNarQt74WDwt';
@@ -18,6 +19,13 @@ export function createPost(newPost) {
   const request = axios.post(`${BASE_URL}/posts${API_KEY}`, newPost);
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+export function fetchPost(postId) {
+  const request = axios.get(`${BASE_URL}/posts/${postId}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-export default (props) => <div>Post details of {props.params.postId}</div>;
+import { fetchPost } from '../actions/index';
+
+class PostDetail extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.postId);
+  }
+
+  render() {
+    const { post } = this.props;
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/">Back</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    post: state.posts.currentPost
+  };
+}
+export default connect(mapStateToProps, { fetchPost })(PostDetail);
